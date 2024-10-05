616728b6c4f024e6b6fd09d3961fdb7876725614
MUKISA Vaniah Christian <vierycalliper@gmail.com>
2024-10-05 18:15:52 +0300

    add frontend url

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,13 @@ const cors = require('cors');
 const Todo = require('./models/Todo');
 
 var app = express();
-app.use(cors());
+
+// CORS configuration to allow requests from your frontend URL
+app.use(cors({
+    origin: 'https://group-bse24-x-todoapp-2-frontend.onrender.com',  // Your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
+
 app.use(express.json());
 
 // Connect to your MongoDB database (replace with your database URL)
