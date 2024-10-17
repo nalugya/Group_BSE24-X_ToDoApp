41e44db95d0c2ff1999977776edf5fb5abba212f
MUKISA Vaniah Christian <vierycalliper@gmail.com>
2024-10-17 12:27:32 +0300

    back

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,8 @@
-// index.js - Serves as the entry point for the React application
+//index.js - Serves as the entry point for the React application
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { getCLS, getFID, getLCP, getTTFB } from 'web-vitals';
-
-// Send metrics to your backend endpoint
-const sendToAnalytics = (metric) => {
-  fetch('https://group-bse24-x-todoapp-2-backend.onrender.com/metrics/frontend', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      name: metric.name,
-      value: metric.value,
-    }),
-  }).catch((error) => console.error('Failed to send metric', error));
-};
-
-// Initialize Web Vitals tracking
-getCLS(sendToAnalytics);
-getFID(sendToAnalytics);
-getLCP(sendToAnalytics);
-getTTFB(sendToAnalytics);
+import App from './App'; 
 
 ReactDOM.render(
   <React.StrictMode>
